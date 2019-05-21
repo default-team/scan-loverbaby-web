4089e72edd12be7c7f31572fa5c479f2aea2d75c
曹远成 <yccao01@mail.nfsq.com.cn>
2019-05-21 22:24:03 +0800

    修复

--- src/main/java/xyz/loverbaby/web/controller/ScanController.java
+++ src/main/java/xyz/loverbaby/web/controller/ScanController.java
@@ -2,10 +2,14 @@ package xyz.loverbaby.web.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import xyz.loverbaby.api.BuildRecordFacade;
 import xyz.loverbaby.api.dto.common.CommonResult;
+import xyz.loverbaby.api.dto.common.Page;
 import xyz.loverbaby.api.dto.request.BuildRecordAddRequest;
+import xyz.loverbaby.api.dto.request.BuildRecordPageRequest;
+import xyz.loverbaby.api.dto.response.BuildRecordVO;
 
 /**
  * @Classname ScanController
@@ -19,6 +23,7 @@ public class ScanController {
     @Autowired
     private BuildRecordFacade buildRecordFacade;
 
+    @PostMapping("/:saveForBuild")
     public CommonResult<Boolean> insertBuildRecord(BuildRecordAddRequest buildRecordAddRequest) {
         BuildRecordAddRequest request = new BuildRecordAddRequest();
         request.setProjectName("gtr");
@@ -29,8 +34,9 @@ public class ScanController {
         CommonResult<Boolean> commonResult = buildRecordFacade.insertBuildRecord(request);
         return commonResult;
     }
-    @GetMapping("get")
-    public CommonResult<Boolean> get() {
-        return buildRecordFacade.get();
+    @GetMapping("/:query")
+    public CommonResult<Page<BuildRecordVO>> get() {
+        BuildRecordPageRequest buildRecordPageRequest = new BuildRecordPageRequest();
+        return buildRecordFacade.listBuildRecordByPage(buildRecordPageRequest);
     }
 }
